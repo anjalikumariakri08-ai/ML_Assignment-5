c4ef4afe0befcec142673a0865f92a3e371fb40b
Anjali Kumari <anjalikumariakri08@gmail.com>
2026-09-23 18:29:24 +0530

    Update student_pass_fail.py

--- Experiment 5/student_pass_fail.py
+++ Experiment 5/student_pass_fail.py
@@ -1,8 +1,5 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-
-# Custom student dataset
-# Features: [Study Hours, Attendance]
 X = np.array([
     [2, 60],
     [3, 65],
@@ -15,20 +12,13 @@ X = np.array([
     [1, 55],
     [4, 68]
 ])
-
 # Target: 0 = Fail, 1 = Pass
 y = np.array([0, 0, 0, 1, 1, 1, 1, 1, 0, 0])
-
-# Create and train Logistic Regression model
 model = LogisticRegression()
 model.fit(X, y)
-
-# Predict for a new student
 study_hours = 6
 attendance = 82
-
 prediction = model.predict([[study_hours, attendance]])
-
 if prediction[0] == 1:
     print("Result: Pass")
 else:
