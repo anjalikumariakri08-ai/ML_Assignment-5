13c05025de4e7538f57a3affbb7d2222fe1cfd2f
Anjali Kumari <anjalikumariakri08@gmail.com>
2026-09-23 18:30:28 +0530

    Update student_roc.py

--- Experiment 5/student_roc.py
+++ Experiment 5/student_roc.py
@@ -2,30 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, roc_auc_score
-
-# Student data
 X = np.array([
     [2, 60], [3, 65], [4, 70], [5, 75], [6, 80],
     [7, 85], [8, 90], [9, 95], [1, 55], [4, 68]
 ])
-
-# 0 = Fail, 1 = Pass
 y = np.array([0, 0, 0, 1, 1, 1, 1, 1, 0, 0])
-
-# Train model
 model = LogisticRegression()
 model.fit(X, y)
-
-# Prediction probability
 y_prob = model.predict_proba(X)[:, 1]
-
-# ROC and AUC
 fpr, tpr, _ = roc_curve(y, y_prob)
 auc = roc_auc_score(y, y_prob)
-
 print("AUC Score:", auc)
-
-# Plot ROC Curve
 plt.plot(fpr, tpr, label="ROC Curve")
 plt.plot([0, 1], [0, 1], "--")
 plt.xlabel("False Positive Rate")
